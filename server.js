cb56752e313c80bc419ae89aa36a880d02eebb64
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-12 15:49:31 +0700

    Get auth user

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
 
 // Listen for requests
 const PORT = process.env.PORT || 5000;
