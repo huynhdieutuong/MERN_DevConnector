9fc71971eab2a41e854d460d8c062e87342ad3f7
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-14 08:26:20 +0700

    Update github repo fetch - replace request with axios

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,5 @@
 const config = require('config');
-const request = require('request');
+const axios = require('axios');
 const { validationResult } = require('express-validator');
 
 const Profile = require('../models/Profile');
@@ -264,27 +264,13 @@ exports.deleteEducation = async (req, res) => {
 // @access  Public
 exports.github = async (req, res) => {
   try {
-    const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
-      method: 'GET',
-      headers: { 'user-agent': 'node.js' },
-    };
+    const result = await axios.get(
+      `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
 
-    request(options, (error, response, body) => {
-      if (error) console.error(error);
-
-      if (response.statusCode !== 200) {
-        return res.status(404).json({ msg: 'No Github profile found' });
-      }
-
-      res.json(JSON.parse(body));
-    });
+    res.json(result.data);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(404).json({ msg: 'No Github profile found' });
   }
 };
