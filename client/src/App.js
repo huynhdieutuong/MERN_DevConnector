84ca6964dfdc691da12c1da4ac4cfbb789fd3241
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-15 11:47:04 +0700

    Added Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,31 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+// Components
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Switch>
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </section>
-      </Switch>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </section>
+        </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
