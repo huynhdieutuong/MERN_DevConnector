bcf1659447527bf7f5ae6092747f7b72baaeaf49
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-16 23:05:48 +0700

    List Experience & Education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
   getCurrentProfile,
@@ -30,6 +32,8 @@ const Dashboard = ({
       {profile ? (
         <Fragment>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
         </Fragment>
       ) : (
         <Fragment>
